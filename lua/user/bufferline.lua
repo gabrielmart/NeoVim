7eb7bc2a0ad4536d4a9488897834d88bb4ce904f
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-05-18 14:45:02 -0300

    improviments

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -81,11 +81,11 @@ bufferline.setup {
       guibg = { attribute = "bg", highlight = "TabLine" },
     },
 
-    buffer_selected = {
-      guifg = {attribute='fg',highlight='#ff0000'},
-      guibg = {attribute='bg',highlight='#0000ff'},
-      gui = 'none'
-      },
+    -- buffer_selected = {
+    --   guifg = {attribute='fg',highlight='#ff0000'},
+    --   guibg = {attribute='bg',highlight='#0000ff'},
+    --   gui = 'none'
+    --   },
     buffer_visible = {
       guifg = { attribute = "fg", highlight = "TabLine" },
       guibg = { attribute = "bg", highlight = "TabLine" },
@@ -148,13 +148,13 @@ bufferline.setup {
     },
 
     separator = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
+      guifg = { attribute = "bg", highlight = "TabLine" },
       guibg = { attribute = "bg", highlight = "TabLine" },
     },
-    separator_selected = {
-      guifg = { attribute = "fg", highlight = "Normal" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
+    -- separator_selected = {
+    --   guifg = { attribute = "fg", highlight = "Normal" },
+    --   guibg = { attribute = "bg", highlight = "Normal" },
+    -- },
     separator_visible = {
       guifg = {attribute='fg',highlight='TabLine'},
       guibg = {attribute='bg',highlight='TabLine'}
