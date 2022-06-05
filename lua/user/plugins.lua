0b540831e82bcba3b6c90b7e6544ece33a68e358
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2022-06-05 01:15:40 -0300

    add tmux

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -118,7 +118,28 @@ return packer.startup(function(use)
 
   -- Terminal
   use { "akinsho/toggleterm.nvim", tag = 'v1.*', config = function() require("toggleterm").setup() end }
-
+  use({
+    "aserowy/tmux.nvim",
+    config = function()
+      require("tmux").setup({
+        -- overwrite default configuration
+        -- here, e.g. to enable default bindings
+        copy_sync = {
+          -- enables copy sync and overwrites all register actions to
+          -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+          enable = true,
+        },
+        navigation = {
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+        },
+        resize = {
+          -- enables default keybindings (A-hjkl) for normal mode
+          enable_default_keybindings = true,
+        }
+      })
+    end
+  })
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
